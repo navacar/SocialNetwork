01ef356e7616b8283236f93d33cc03ead8570227
navacar <55153165+navacar@users.noreply.github.com>
2020-06-13 19:49:02 +0200

    Dialogs TextArea + Button

--- src/components/ Dialogs/Dialogs.jsx
+++ src/components/ Dialogs/Dialogs.jsx
@@ -3,12 +3,20 @@ import './Dialogs.css'
 import { NavLink } from 'react-router-dom';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import { createRef } from 'react';
 
 const Dialogs = (props) => {
 
     let DialogsElements = props.State.DialogData.map(Dialog => (<DialogItem name={Dialog.name} id={Dialog.id} />));
 
     let MessageElements = props.State.MessageData.map(message => <Message message={message.message} />);
+
+    let MessageTextRef = React.createRef()
+
+    let sendMessage = () => {
+        let MessageText = MessageTextRef.current.value
+        alert(MessageText)
+    }
 
     return (
         <div className='dialogs'>
@@ -17,6 +25,14 @@ const Dialogs = (props) => {
             </div>
             <div className='messages'>
                 {MessageElements}
+                <div className='SendArea'>
+                    <div>
+                        <textarea ref={MessageTextRef}></textarea>
+                    </div>
+                    <div>
+                        <button onClick={sendMessage}>Send Message</button>
+                    </div>
+                </div>
             </div>
         </div>
 
