66876472cc12b63ac8de6d2464440f5949f8963c
navacar <55153165+navacar@users.noreply.github.com>
2020-06-17 14:23:11 +0200

    #37 store + OOP
    Bind
    id
    unshift

--- src/State/State.js
+++ src/State/State.js
@@ -1,54 +1,52 @@
-
-
-let rerenderEntireTree = () => {
-
-}
-
-let State = {
-    ProfilePage: {
-        Posts: [
-            {id: 1, message: "Hi, how are you?", likeCount: 100},
-            {id: 2, message: "It's my first post!", likeCount: 59}
-        ],
-        newPostText: '',
+let Store = {
+    _State: {
+        ProfilePage: {
+            Posts: [
+                {id: 1, message: "Hi, how are you?", likeCount: 100},
+                {id: 2, message: "It's my first post!", likeCount: 59}
+            ],
+            newPostText: '',
+        },
+        DialogsPage: {
+            DialogData: [
+                { id: 1, name: 'Alexandr' },
+                { id: 2, name: 'Misha' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Valera' },
+                { id: 5, name: 'Maria' },
+                { id: 6, name: 'Andrew' }
+            ],
+            MessageData: [
+                { message: 'Hi' },
+                { message: 'How are you?' },
+                { message: "I'm fine thank you" } 
+            ] 
+        }
     },
-    DialogsPage: {
-        DialogData: [
-            { id: 1, name: 'Alexandr' },
-            { id: 2, name: 'Misha' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Valera' },
-            { id: 5, name: 'Maria' },
-            { id: 6, name: 'Andrew' }
-        ],
-        MessageData: [
-            { message: 'Hi' },
-            { message: 'How are you?' },
-            { message: "I'm fine thank you" } 
-        ] 
+    _callSubscriber() {
+    },
+    ChangeNewPostText(Text) {
+        this._State.ProfilePage.newPostText = Text
+        this._callSubscriber()
+    },
+    addPost() {
+
+        let newPost = {
+            id: Store._State.ProfilePage.Posts[Store._State.ProfilePage.Posts.length - 1].id + 1,
+            message: Store._State.ProfilePage.newPostText,
+            likeCount: 0
+        }; 
+    
+        this._State.ProfilePage.Posts.unshift(newPost)
+        this._State.ProfilePage.newPostText = ''
+        this._callSubscriber()
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
+    },
+    getState() {
+        return (this._State)
     }
 }
 
-export const ChangeNewPostText = (Text) => {
-    State.ProfilePage.newPostText = Text
-    rerenderEntireTree(State, addPost, ChangeNewPostText)
-}
-
-export const addPost = () => {
-
-    let newPost = {
-        id: 3,
-        message: State.ProfilePage.newPostText,
-        likeCount: 0
-    }; 
-
-    State.ProfilePage.Posts.push(newPost)
-    State.ProfilePage.newPostText = ''
-    rerenderEntireTree(State, addPost, ChangeNewPostText)
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
-
-export default State;
+export default Store;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import './index.css';
-import State, { ChangeNewPostText, subscribe } from './State/State';
-import {addPost} from './State/State';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Store from './State/State';
 
-let rerenderEntireTree = (State, addPost, ChangeNewPostText) => {
-    ReactDOM.render(<App State={State} addPost={addPost} ChangeNewPostText={ChangeNewPostText}/>, document.getElementById('root'));
+
+let rerenderEntireTree = () => {
+    ReactDOM.render(<App 
+        State={Store.getState()} 
+        addPost={Store.addPost.bind(Store)} 
+        ChangeNewPostText={Store.ChangeNewPostText.bind(Store)} 
+        />, document.getElementById('root'));
 }
 
-rerenderEntireTree(State, addPost, ChangeNewPostText)
-subscribe(rerenderEntireTree)
+rerenderEntireTree()
+Store.subscribe(rerenderEntireTree)
 
 serviceWorker.unregister();
